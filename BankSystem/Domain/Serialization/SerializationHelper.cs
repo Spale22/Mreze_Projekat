13702186614788ab72893a924f43dd1754c444a8
Veljko Spasic <spasicveljko22@gmail.com>
2026-01-24 19:31:39 +0100

    Add SerializationHelper for binary object serialization
    - Added SerializationHelper.cs with generic Serialize and Deserialize methods using BinaryFormatter.

---
+++ BankSystem/Domain/Serialization/SerializationHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Domain.Serialization
+{
+    public static class SerializationHelper
+    {
+        public static byte[] Serialize<T>(T obj)
+        {
+            byte[] dataBuffer;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(ms, obj);
+                dataBuffer = ms.ToArray();
+                return dataBuffer;
+            }
+        }
+        public static T Deserialize<T>(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                object obj = bf.Deserialize(ms);
+                return (T)obj;
+            }
+        }
+    }
+}
