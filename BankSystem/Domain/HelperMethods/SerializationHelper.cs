8e7ae3ab879a961ca8c2474bee645e9444a10e79
Veljko Spasic <spasicveljko22@gmail.com>
2026-01-25 22:51:24 +0100

    reafactor(Server, Domain): refactor server to use a single listening socket, multiplex client connections, and dispatch requests based on PackageType. - Introduce PackageType enum for protocol framing and update SerializationHelper to include package type in serialized data. -  Centralize request handling and update handlers to use the new serialization format.

--- BankSystem/Domain/HelperMethods/SerializationHelper.cs
+++ BankSystem/Domain/HelperMethods/SerializationHelper.cs
@@ -1,28 +1,31 @@
-﻿using System.IO;
+﻿using Domain.Enumerations;
+using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Domain.HelperMethods
 {
     public static class SerializationHelper
     {
-        public static byte[] Serialize<T>(T obj)
+        public static byte[] Serialize<T>(PackageType pkgType , T obj)
         {
             byte[] dataBuffer;
             using (MemoryStream ms = new MemoryStream())
             {
+                ms.WriteByte((byte)pkgType);
                 BinaryFormatter bf = new BinaryFormatter();
                 bf.Serialize(ms, obj);
                 dataBuffer = ms.ToArray();
                 return dataBuffer;
             }
         }
-        public static T Deserialize<T>(byte[] data)
+        public static (PackageType, T) Deserialize<T>(byte[] data)
         {
             using (MemoryStream ms = new MemoryStream(data))
             {
+                var pkgType = (PackageType)ms.ReadByte();
                 BinaryFormatter bf = new BinaryFormatter();
                 object obj = bf.Deserialize(ms);
-                return (T)obj;
+                return (pkgType, (T)obj);
             }
         }
     }
