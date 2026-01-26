b44ef4bdfd8a25faab393022216f7eafbf03f372
saraa31 <sarita.pavlovic2004@gmail.com>
2026-01-26 12:59:39 +0100

    refactor(BranchOffice): forward UDP client requests to server via TCP

--- BankSystem/BranchOffice/BranchOffice.cs
+++ BankSystem/BranchOffice/BranchOffice.cs
@@ -1,9 +1,8 @@
-﻿using Domain.DTOs;
-using Domain.HelperMethods;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
+using System.Threading;
 
 namespace BranchOffice
 {
@@ -11,45 +10,83 @@ namespace BranchOffice
     {
         static void Main(string[] args)
         {
+            Run();
+        }
+
+        static void Run()
+        {
             Socket branchSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             IPEndPoint localEP = new IPEndPoint(IPAddress.Loopback, 16001);
             branchSocket.Bind(localEP);
-
             branchSocket.Blocking = false;
+
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 15000);
+
+            var cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Shutdown requested (Ctrl+C).");
+            };
+
+            int maxClients = 10;
+
+            try
+            {
+                serverSocket.Connect(serverEP);
+                Console.WriteLine("Connected to server successfully.");
+                serverSocket.Blocking = false;
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"Failed to connect to server at startup.\nError:{ex.Message}");
+            }
 
             int timeout = 500 * 1000;
 
-            while(true)
+            EndPoint clientEP = null;
+
+            while (!cts.IsCancellationRequested)
             {
-                if(branchSocket.Poll(timeout, SelectMode.SelectRead))
-                {
-                    try
-                    {
-                        byte[] buffer = new byte[8192];
-                        EndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
-                        int bytesReceived = branchSocket.ReceiveFrom(buffer, ref clientEP);
-
-                        byte[] data = new byte[bytesReceived];
-                        Array.Copy(buffer, data, bytesReceived);
-
-                        ClientDataRequestDTO request = SerializationHelper.Deserialize<ClientDataRequestDTO>(data);
-                        Console.WriteLine($"Request from {clientEP}: Operation: {request.Operation} Amount={request.Amount}");
-
-                        ClientDataResponseDTO response = new ClientDataResponseDTO
-                        {
-                            Success = true,
-                            Message = $"Processed {request.Operation} of amount {request.Amount}"
-                        };
-
-                        byte[] responseBytes = SerializationHelper.Serialize(response);
-                        branchSocket.SendTo(responseBytes, clientEP);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Socket error on {(IPEndPoint)branchSocket.LocalEndPoint}: {ex.Message}");
-                    }
-                }
+                Polling(serverSocket, branchSocket, timeout, ref clientEP);
+                Multiplexing(serverSocket, branchSocket, maxClients, timeout, ref clientEP);
             }
+            branchSocket.Close();
+            serverSocket.Close();
+            Console.WriteLine("Branch office shutting down...");
+            Console.ReadKey();
+        }
+
+        private static void Polling(Socket serverSocket, Socket branchSocket, int timeout, ref EndPoint clientEP)
+        {
+            try
+            {
+                byte[] buffer = new byte[8192];
+                EndPoint tempEP = new IPEndPoint(IPAddress.Any, 0);
+
+                if (branchSocket.Poll(timeout, SelectMode.SelectRead))
+                {
+                    int bytesReceived = branchSocket.ReceiveFrom(buffer, ref tempEP);
+                    if (bytesReceived == 0)
+                        return;
+                    clientEP = tempEP;
+                    byte[] data = new byte[bytesReceived];
+                    Buffer.BlockCopy(buffer, 0, data, 0, bytesReceived);
+                    serverSocket.Send(data);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        private static void Multiplexing(Socket serverSocket, Socket branchSocket, int maxClients, int timeout, ref EndPoint clientEP)
+        {
+            
         }
     }
 }
